8f2082ce96bd7ad09c259f8309ed3e492407854a
Jac123-glitch <moreno.jacqueline@student.ccm.edu>
2026-09-08 14:41:15 -0400

    Week1-lab2

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,16 +1,46 @@
-package Labs.Week_1.scr;
+
 import java.util.Scanner;
 
 public class SalesBarChart {
-public static void main(String [] args){
+public static void main (String [] args){
+Scanner keyboard = new Scanner(System.in);
+int num_stores = 5;
+int num_asterisk=0;
+int [] sales = new int[num_stores];
+for(int i = 0; i < num_stores; i++){
+    System.out.print("Enter today's sales for stores "+(i+1)+": ");
+    sales [i] = keyboard.nextInt();
+    num_asterisk = sales[i]/100;
+}
+    System.out.println("\nSales Bar Chart");
+    System.out.println("(Each * = $ 100)");
 
-Scanner keyboard = new Scanner (System.in);
 
-int numA,numSales;
+for(int i = 0; i < num_stores; i++){
+    System.out.print("Stores "+(i+1)+": ");
+     
+}
+for(int j = 0; j < num_asterisk; j++){ 
+    System.out.println("*");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
 }
 
     
-}
+
