b71d8691dceea2f26a6b7e9d7444aba42eac297e
Jac123-glitch <moreno.jacqueline@student.ccm.edu>
2026-09-08 18:48:23 -0400

    Week1-Pattern-week2

--- Patterns.java
+++ Patterns.java
@@ -1,17 +1,31 @@
-package Labs.Week_1.scr;
+
 
 public class Patterns {
     public static void main(String [] args){
-
-
-
-
-
+    int num_row = 10;
+    for (int i = 1; i<= num_row;i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print("+");
+        }
+        System.out.println();
+    }
+    
+    System.out.println();
+    
+    for (int i = num_row; i>=1;i--){
+        for(int j = 1; j<=i; j++){
+            System.out.print("+");
+        }
+        System.out.println();
     }
 
 
 
+    
 
+
+
+}
 
 
     
