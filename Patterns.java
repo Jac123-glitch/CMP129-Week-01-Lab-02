ca054987692f987a25c7b8396b528e6fca9fc423
Jac123-glitch <moreno.jacqueline@student.ccm.edu>
2026-09-08 18:59:05 -0400

    Week1-Pattern-lab2

--- Patterns.java
+++ Patterns.java
@@ -3,6 +3,7 @@
 public class Patterns {
     public static void main(String [] args){
     int num_row = 10;
+    //Pattern A that increase plus sign 
     for (int i = 1; i<= num_row;i++){
         for(int j = 1; j<=i; j++){
             System.out.print("+");
@@ -11,7 +12,7 @@ public class Patterns {
     }
     
     System.out.println();
-    
+    // Pattern B decreasing plus sign
     for (int i = num_row; i>=1;i--){
         for(int j = 1; j<=i; j++){
             System.out.print("+");
